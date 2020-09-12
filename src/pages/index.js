7a19ad9ed7a6b6fe239fa33eba3daad1145bd25b
Stephen Algeo <stalgeo@whm0005845.lan>
2020-09-12 15:44:41 +0100

    Add my basic info

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <h1>Hi, I'm Stephen Algeo.</h1>
+    <p>I'm a Software Engineer and Property Investor.</p>
+    <p>
+      I use mine and other people's money to buy refurbish and hold property in
+      Leeds and Liverpool.
+    </p>
+    {/* <Link to="/">Go back to the homepage</Link> */}
   </Layout>
 )
 
